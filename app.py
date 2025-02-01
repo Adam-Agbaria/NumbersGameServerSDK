3ff2a41d8f4f363f90ee4ddb7be894ac89494146
Adam Agbaria <adam.agbaria@s.afeka.ac.il>
2025-02-01 18:02:12 +0200

    Added route

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enables CORS for all routes
+
 from routes.game_routes import game_blueprint
 from routes.round_routes import round_blueprint
 
-app = Flask(__name__)
-
-# Register Blueprints
 app.register_blueprint(game_blueprint, url_prefix='/game')
 app.register_blueprint(round_blueprint, url_prefix='/round')
 
