77473720c12bbe959710b36ea96cdd50a477a45e
Adam Agbaria <adam.agbaria@s.afeka.ac.il>
2025-02-01 18:04:55 +0200

    Added route

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enables CORS for all routes
-
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "https://numbers-game-web-app.vercel.app",
+            "http://localhost:5500",   # Allow local testing
+            "http://127.0.0.1:5000",   # Allow Flask local server
+            "*",  # If you want to allow all origins (less secure)
+        ],
+        "methods": ["GET", "POST", "OPTIONS"],  # Allowed methods
+        "allow_headers": ["Content-Type", "Authorization"]
+    }
+})
 from routes.game_routes import game_blueprint
 from routes.round_routes import round_blueprint
 
